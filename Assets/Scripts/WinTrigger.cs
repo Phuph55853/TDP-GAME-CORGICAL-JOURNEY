f0d3c2356616bc7507f0b91c7760ec919d12a574
Phu67 <phutd67@gmail.com>
2025-06-22 13:06:15 +0700

    15 Level

---
+++ Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class WinTrigger : MonoBehaviour
+{
+    public GameObject winPanel; // Kéo WinPanel vào đây trong Inspector
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            winPanel.SetActive(true);
+            Time.timeScale = 0f; // Dừng game nếu muốn
+        }
+    }
+}
